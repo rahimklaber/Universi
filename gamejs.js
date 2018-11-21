3a61d5cb803e54512f7dcca0852d84c514649c72
Rahim Klaber <rahimklaber2@gmail.com>
2018-11-21 23:42:45 +0100

    you can now place pieces

--- gamejs.js
+++ gamejs.js
@@ -8,7 +8,7 @@ function generateBoard(){
         $("#board-table").append("<tr id=row"+i+""+"><tr")
         for(var j=0;j<8;j++){
             $("#row"+i).append("<td id=cell_"+i+""+j+"></td>")
-            $("#cell_"+i+""+j).append("<canvas height=100% onclick=handleclick(i,j) onmouseover=handlehover(i,j) width =100% id=canv_"+i+""+j+"></canvas>")
+            $("#cell_"+i+""+j).append("<canvas height=100% onclick=handleclick("+i+","+j+") onmouseover=handlehover(i,j) width =100% id=canv_"+i+""+j+"></canvas>")
         }
     }
 }
@@ -22,4 +22,12 @@ function draw(){
             ctx.fillRect(0,0,100,100)
         }
     }
+}
+function handleclick(i,j){
+    var canvas = $("#canv_"+i+""+j)[0]
+    var ctx = canvas.getContext("2d")
+    ctx.beginPath()
+    ctx.fillStyle="black"
+    ctx.arc(50,50,50,0,2*Math.PI)
+    ctx.fill()
 }
