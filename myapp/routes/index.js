56b152a88fcd152d1cf924aa31108e1aa4ce529a
RowanRichie <rowanr99@gmail.com>
2018-11-30 22:00:50 +0100

    -

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -3,12 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/splash", function (req, res) {
-    res.sendFile("splash.html", {root: "./public"});
+    res.sendFile("splash.html", {root: "./myapp"});
 });
 
 /* Pressing the 'PLAY' button, returns this page */
 router.get("/play", function(req, res) {
-    res.sendFile("game.html", {root: "./public"});
+    res.sendFile("game.html", {root: "./myapp"});
 });
 
 module.exports = router;
