962210ca9ae92e89c18a360a6473c7a733d3ad17
RowanRichie <rowanr99@gmail.com>
2018-11-30 22:08:34 +0100

    -

--- myapp/app.js
+++ myapp/app.js
@@ -2,7 +2,9 @@ var express = require("express");
 var http = require("http");
 
 var indexRouter = require("./routes/index");
-app.get("/splash", indexRouter);
+app.get("/play", indexRouter);
+app.get("/", (req, res));
+
 
 var port = process.argv[2];
 var app = express();
