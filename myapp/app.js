368aee1af010e9c1b419e0a9effa889f40915358
RowanRichie <rowanr99@gmail.com>
2018-11-30 22:20:02 +0100

    -

--- myapp/app.js
+++ myapp/app.js
@@ -4,5 +4,11 @@ var http = require("http");
 var port = process.argv[2];
 var app = express();
 
+
+router.get("/splash", function (req, res) {
+    res.sendFile("splash.html", {root: "./public"});
+});
+
+
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
