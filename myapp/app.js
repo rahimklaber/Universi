e728cd27a61a410a6cb8a8727274ad95dd7ac3b5
RowanRichie <rowanr99@gmail.com>
2018-11-30 22:25:37 +0100

    -

--- myapp/app.js
+++ myapp/app.js
@@ -1,15 +1,8 @@
-
 var express = require("express");
-var router = express.Router();
+var http = require("http");
 
-/* GET home page. */
-router.get("/splash", function (req, res) {
-    res.sendFile("splash.html", {root: "./public"});
-});
+var port = process.argv[2];
+var app = express();
 
-/* Pressing the 'PLAY' button, returns this page */
-router.get("/play", function(req, res) {
-    res.sendFile("game.html", {root: "./public"});
-});
-
-module.exports = router;
+app.use(express.static(__dirname + "/public"));
+http.createServer(app).listen(port);
