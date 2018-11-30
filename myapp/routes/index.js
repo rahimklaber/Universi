62160cda40a5e8b0c2b289ba5d296373fa56d7fa
RowanRichie <rowanr99@gmail.com>
2018-11-30 21:49:43 +0100

    *.html removed - routes added

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/splash', function(req, res) {
+  res.sendFile("splash.html", {root: "./public"});
+});
+
+/* After the user clicks on Play */
+router.get("/play", function(req, res) {
+  res.sendFile("game.html", {root: "./public"});
 });
 
 module.exports = router;
