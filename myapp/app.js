81bc544a00da3f9d502a1a63eb48fc6cff3baeee
RowanRichie <rowanr99@gmail.com>
2018-11-30 22:17:42 +0100

    -

--- myapp/app.js
+++ myapp/app.js
@@ -1,11 +1,6 @@
 var express = require("express");
 var http = require("http");
 
-app.get("/test", function(req, res) {
-    res.render('splash.html')
-});
-
-
 var port = process.argv[2];
 var app = express();
 
